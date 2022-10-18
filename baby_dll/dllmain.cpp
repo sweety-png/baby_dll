232dfe8a258c89c315f266be6604ee8e1ef9c710
sweety <josecastillomartinez201033@gmail.com>
2022-10-18 18:27:16 +0200

    Changed some code
    :smile:

--- baby_dll/dllmain.cpp
+++ baby_dll/dllmain.cpp
@@ -2,9 +2,20 @@
 
 BOOL APIENTRY DllMain(HMODULE hModule, DWORD ul_reason_for_call, LPVOID lpReserved)
 {
-    if (ul_reason_for_call == DLL_PROCESS_ATTACH)
+    switch (ul_reason_for_call)
     {
-        MessageBox(0, "baby_dll made by sweety#3848", "sweety#3848", MB_ICONINFORMATION);
-        return TRUE;
+        case DLL_PROCESS_ATTACH:
+            MessageBox(0, "baby_dll made by sweety#3848", "Injected", MB_OK | MB_ICONINFORMATION);
+            break;
+        case DLL_THREAD_ATTACH:
+            MessageBox(0, "baby_dll made by sweety#3848", "Injected", MB_OK | MB_ICONINFORMATION);
+            break;
+        case DLL_THREAD_DETACH:
+            MessageBox(0, "baby_dll made by sweety#3848", "Injected", MB_OK | MB_ICONINFORMATION);
+            break;
+        case DLL_PROCESS_DETACH:
+            MessageBox(0, "baby_dll made by sweety#3848", "Injected", MB_OK | MB_ICONINFORMATION);
+            break;
     }
+    return TRUE;
 }
